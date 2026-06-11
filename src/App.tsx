7bfba20512205e45930dd997f56c1188f2c2aaa5
Bradley Hultzer <brad.hultzer@gmail.com>
2026-06-11 08:33:26 +0200

    Visual tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <div className="mx-auto flex min-h-full w-full max-w-md flex-col">
-        <main className="flex-1 pl-[calc(env(safe-area-inset-left)_+_1rem)] pr-[calc(env(safe-area-inset-right)_+_1rem)] pt-[calc(env(safe-area-inset-top)_+_2rem)] pb-[calc(env(safe-area-inset-bottom)_+_2rem)]">
+        <main className="flex-1 pl-[calc(env(safe-area-inset-left)_+_1rem)] pr-[calc(env(safe-area-inset-right)_+_1rem)] pt-[calc(env(safe-area-inset-top)_+_1rem)] pb-[calc(env(safe-area-inset-bottom)_+_2rem)]">
           <Suspense fallback={null}>
             <Routes>
               <Route path="/" element={<Dashboard />} />
